5854dcd9048c6e9a6eb69bb0f87571a97477b28c
davidbuckley32 <83192116+davidbuckley32@users.noreply.github.com>
2021-06-07 20:26:22 -0400

    third python assignment

---
+++ assignment3.py
@@ -0,0 +1,9 @@
+print("Hello World")
+name = "David"
+print("Hello",name)
+print("Hello " + name)
+num = 6
+print("Hello", num)
+# print("Hello ") + num intentional error message
+# Ninja Bonus 1 saving for later
+# Ninja Bonus 2 saving for later

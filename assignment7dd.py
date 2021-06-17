39582ba6e2a4e31cfad49c00524ea97887a06175
davidbuckley32 <83192116+davidbuckley32@users.noreply.github.com>
2021-06-16 20:30:10 -0400

    finished version of 7th python assignment, part 4

--- assignment7dd.py
+++ assignment7dd.py
@@ -1,14 +1,15 @@
 def values_greater_than_second():
     global x
-    listA = (input("Enter a list of numbers here:").split()
+    listA = (input("Enter a list of numbers here:").split())
+    listA = [int(i) for i in listA]
     secondNum = listA[1]
     secondList = []
     x = len(listA)
-    def newListFormation(listA):
+    def newListFormation(secondList):
         global x
         counter = 0
         greaterCounter = 0
-        while x != 0:
+        while x != 1:
             if x < 2:
                 return "False"
             elif listA[counter] > listA[1]:
@@ -16,7 +17,11 @@ def values_greater_than_second():
                 greaterCounter = greaterCounter + 1
                 counter = counter + 1
                 x = x - 1
+            else:
+                counter = counter + 1
+                x = x - 1
         print (greaterCounter)
         return secondList
-    newListFormation(listA)
+    newListFormation(secondList)
+    return secondList
 values_greater_than_second()

69e8c2e94cfb946ad24851ca65cf7644c13f2b3c
davidbuckley32 <83192116+davidbuckley32@users.noreply.github.com>
2021-06-28 00:18:30 -0400

    1.update

--- alpha/alpha1app/views.py
+++ alpha/alpha1app/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render, HttpResponse
-def index(request):\
+from django.shortcuts import render, HttpResponse, redirect
+def index(request):
     return HttpResponse("placeholder to later display a list of all blogs")
 
-def new(request):\
+def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    response = create('')
+    return response
+
+def show(request, number):
+    number = str(number)
+    return HttpResponse("placeholder to display blog number " + number)
 # Create your views here.

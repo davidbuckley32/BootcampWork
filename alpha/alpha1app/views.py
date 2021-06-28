ccdb48a4db5b47a9dc998eb31feab5b47fc14f55
davidbuckley32 <83192116+davidbuckley32@users.noreply.github.com>
2021-06-27 23:37:31 -0400

    created

---
+++ alpha/alpha1app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttpResponse
+def index(request):\
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):\
+    return HttpResponse("placeholder to display a new form to create a new blog")
+# Create your views here.

95f9864d5f940cb857c657704f49baf938986578
davidbuckley32 <83192116+davidbuckley32@users.noreply.github.com>
2021-07-15 21:03:11 -0400

    updated

--- dragonProj/dragonApp/urls.py
+++ dragonProj/dragonApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index), #localhost8000/
     path('dragons', views.show_dragons), #localhost8000/ + dragons
+    path('dragons/new', views.new_dragons), #localhost8000/ + dragons + new
     path('dragons/create', views.create_dragons), #localhost8000/ + dragons + create
-    path('dragons/<int:dragon_id>/edit', views.edit_dragon),
+    path('dragons/<int:dragon_id>/edit', views.edit_dragons),
 ]
--- dragonProj/dragonApp/views.py
+++ dragonProj/dragonApp/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render, HttpResponse, redirect
 
-# Create your views here.
+#/
 def index(request):
     return HttpResponse("Here there be dragons!")
 
+#/dragons (show dragons here)
 def show_dragons(request):
-    return render(request, "index.html")
+    context = {
+        "all_dragons":request.POST['dragon_name']
+    }
+    return render(request, "show_dragons.html", context)
+    # return render(request, "show_dragons.html")
     # return HttpResponse("This is a list of all dragons.")
 
-def edit_dragon(request, dragon_id):
+#/dragons/new (Submit FORM HERE) HOLDS form
+def new_dragons(request):
+    return render(request, "new_dragons.html")
+
+#/dragons/create (Create Dragons here)
+def create_dragons(request):
+    if request.method == "POST":
+        print("Dragon Created!")
+        return redirect("/dragons")
+    return redirect("/dragons/new")
+
+#/dragons/<dragon_id>/edit
+def edit_dragons(request, dragon_id):
     context = {
         "dragon_id":dragon_id,
+        "dragon_colors":['black', 'red', 'blue', 'green', 'white']
     }
     return render(request, "edit.html", context)
     # return HttpResponse(f"Edit Dragon with id {dragon_id}") #why do you need the f
-
-def create_dragons(request):
-    #this is where we create a dragon
-    return redirect("/dragons")

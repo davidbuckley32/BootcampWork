e9c4713ff47c20bc64c43fb10f12c81f69e49353
davidbuckley32 <83192116+davidbuckley32@users.noreply.github.com>
2021-07-28 20:08:04 -0400

    created

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+name = input ("What is your name?")
+age = input ("What is your age?")
+num1 = 100
+year2020 = 2020
+yearsLeft = num1 - int(age)
+yearAge = year2020 + yearsLeft
+print("Dear {name}, you will turn 100 years old in {yearAge}.

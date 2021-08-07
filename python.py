f6040d4aa998cdc3cd7a702dfb1ed03f28d250d1
davidbuckley32 <83192116+davidbuckley32@users.noreply.github.com>
2021-08-06 21:03:59 -0400

    updated

--- python.py
+++ python.py
@@ -2,7 +2,9 @@ firstText = "This is a sentence that I am typing in Python."
 secondText = "This is the second sentence that I am typing in Python."
 thirdText = "This is the third sentence that I am typing in Python."
 fourthText = "This is the fourth sentence that I am typing in Python."
+fifthText = "This is the fifth sentence that I am typing in Python."
 print(firstText)
 print(secondText)
 print(thirdText)
 print(fourthText)
+print(fifthText)

b095816d7c61bf110e3a010b3ddb2977ab5f5024
davidbuckley32 <83192116+davidbuckley32@users.noreply.github.com>
2021-08-11 22:46:04 -0400

    updated

--- python.py
+++ python.py
@@ -6,6 +6,7 @@ fifthText = "This is the fifth sentence that I am typing in Python."
 sixthText = "This is the sixth sentence that I am typing in Python."
 seventhText = "This is the seventh sentence that I am typing in Python."
 eigthText = "This is the seventh sentence that I am typing in Python."
+ninthText = "This is the ninth sentence that I am typing in Python."
 print(firstText)
 print(secondText)
 print(thirdText)
@@ -14,3 +15,4 @@ print(fifthText)
 print(sixthText)
 print(seventhText)
 print(eigthText)
+print(ninthText)
